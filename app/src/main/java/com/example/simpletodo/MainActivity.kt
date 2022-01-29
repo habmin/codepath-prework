0b0aee68275e0bdeaff17b3eaeb58499a878e10d
Henry A Baum <henry.baum46@myhunter.cuny.edu>
2022-01-29 02:04:25 -0500

    Long press on list item removes from list

--- app/src/main/java/com/example/simpletodo/MainActivity.kt
+++ app/src/main/java/com/example/simpletodo/MainActivity.kt
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     val listOfTasks = mutableListOf<String>()
+    lateinit var adapter: TaskItemAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,12 +24,28 @@ class MainActivity : AppCompatActivity() {
         listOfTasks.add("Do laundry")
         listOfTasks.add("Read")
 
+        val onLongClickListener = object: TaskItemAdapter.OnLongClickListener {
+            override fun onItemLongClicked(position: Int) {
+                listOfTasks.removeAt(position)
+            adapter.notifyDataSetChanged()
+            }
+        }
+
+        //To Do List set up as RecyclerView
         val recyclerView = findViewById<RecyclerView>(R.id.toDoList)
-
-        val adapter = TaskItemAdapter(listOfTasks)
-
+        adapter = TaskItemAdapter(listOfTasks, onLongClickListener)
         recyclerView.adapter = adapter
-
         recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val textInputField = findViewById<EditText>(R.id.addTaskField)
+
+        //button on press listener (submit)
+        findViewById<Button>(R.id.button).setOnClickListener {
+            val textString = textInputField.text.toString()
+            listOfTasks.add(textString)
+            //Notify adapter that data is updated
+            adapter.notifyItemInserted(listOfTasks.size - 1)
+            textInputField.setText("")
+        }
     }
 }
--- app/src/main/java/com/example/simpletodo/TaskItemAdapter.kt
+++ app/src/main/java/com/example/simpletodo/TaskItemAdapter.kt
@@ -1,13 +1,20 @@
 package com.example.simpletodo
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class TaskItemAdapter(val listOfItems: List<String>) :
+class TaskItemAdapter(val listOfItems: List<String>,
+                      val longClickListener: OnLongClickListener) :
     RecyclerView.Adapter<TaskItemAdapter.ViewHolder>() {
+
+    interface OnLongClickListener {
+        fun onItemLongClicked(position: Int)
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val context = parent.context
         val inflater = LayoutInflater.from(context)
@@ -23,11 +30,17 @@ class TaskItemAdapter(val listOfItems: List<String>) :
     override fun getItemCount(): Int {
         return listOfItems.size
     }
+
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val textView: TextView
 
         init {
             textView = itemView.findViewById(android.R.id.text1)
+
+            itemView.setOnLongClickListener {
+                longClickListener.onItemLongClicked(adapterPosition)
+                true
+            }
         }
     }
 }
