4ec871ba092661096c0894d00eca979c5d5c94cc
Henry A Baum <henry.baum46@myhunter.cuny.edu>
2022-01-29 01:39:02 -0500

    Render list items using TakItemAdapter

--- app/src/main/java/com/example/simpletodo/MainActivity.kt
+++ app/src/main/java/com/example/simpletodo/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.simpletodo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    val listOfTasks = mutableListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        Console log function for button press
+//        findViewById<Button>(R.id.button).setOnClickListener{
+//            Log.i("Test", "Button Was Pressed!")
+//        }
+
+        listOfTasks.add("Do laundry")
+        listOfTasks.add("Read")
+
+        val recyclerView = findViewById<RecyclerView>(R.id.toDoList)
+
+        val adapter = TaskItemAdapter(listOfTasks)
+
+        recyclerView.adapter = adapter
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
