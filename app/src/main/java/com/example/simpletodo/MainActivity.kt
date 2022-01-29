da3b2f7fd1499c9b9292d867b0e5348704c3a0ad
Henry A Baum <henry.baum46@myhunter.cuny.edu>
2022-01-29 02:17:46 -0500

    App reads and writes to do list to text file

--- app/src/main/java/com/example/simpletodo/MainActivity.kt
+++ app/src/main/java/com/example/simpletodo/MainActivity.kt
@@ -7,10 +7,14 @@ import android.widget.Button
 import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import org.apache.commons.io.FileUtils
+import java.io.File
+import java.io.IOException
+import java.nio.charset.Charset
 
 class MainActivity : AppCompatActivity() {
 
-    val listOfTasks = mutableListOf<String>()
+    var listOfTasks = mutableListOf<String>()
     lateinit var adapter: TaskItemAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,16 +25,14 @@ class MainActivity : AppCompatActivity() {
 //            Log.i("Test", "Button Was Pressed!")
 //        }
 
-        listOfTasks.add("Do laundry")
-        listOfTasks.add("Read")
-
         val onLongClickListener = object: TaskItemAdapter.OnLongClickListener {
             override fun onItemLongClicked(position: Int) {
                 listOfTasks.removeAt(position)
-            adapter.notifyDataSetChanged()
+                adapter.notifyDataSetChanged()
+                saveItems()
             }
         }
-
+        loadItems()
         //To Do List set up as RecyclerView
         val recyclerView = findViewById<RecyclerView>(R.id.toDoList)
         adapter = TaskItemAdapter(listOfTasks, onLongClickListener)
@@ -46,6 +48,27 @@ class MainActivity : AppCompatActivity() {
             //Notify adapter that data is updated
             adapter.notifyItemInserted(listOfTasks.size - 1)
             textInputField.setText("")
+            saveItems()
+        }
+    }
+    //Reading and writing to file
+    fun getDataFile(): File {
+        return File(filesDir, "todolist.txt")
+    }
+
+    fun saveItems() {
+        try {
+            FileUtils.writeLines(getDataFile(), listOfTasks)
+        } catch (ioException: IOException) {
+            ioException.printStackTrace()
+        }
+    }
+
+    fun loadItems() {
+        try {
+            listOfTasks = FileUtils.readLines(getDataFile(), Charset.defaultCharset())
+        } catch (ioException: IOException) {
+            ioException.printStackTrace()
         }
     }
 }
